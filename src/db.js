2950fd2eaa5686fc468e252186b2d9401ce737bb
ZeroSwordDev <Miguelworking1@gmail.com>
2023-04-28 19:52:54 -0400

    Proyecto Actualizado

--- src/db.js
+++ src/db.js
@@ -1,15 +1,24 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_DATABASE
+  DB_USER, DB_PASSWORD, DB_HOST,DB_DATABASE, DB_DEPLOY
 } = process.env;
 
 
-const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/railway`, {
+/* const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/railway`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  port: 7271 // Agregas el puerto aquí
+}); */
+
+const sequelize = new Sequelize(`${DB_DEPLOY}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   port: 7271 // Agregas el puerto aquí
 });
+
+
+
 /* const basename = path.basename(__filename);
 
 const modelDefiners = [];
